eab649bfd8ae3bc19737a7fe3d4a8634cbaec828
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-06-14 14:37:36 -0500

    provided store, use dispatch in funct&class comp

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,83 @@
-import classes from './Counter.module.css';
+import classes from './Counter.module.css'
+import {useSelector, useDispatch} from 'react-redux'
 
-const Counter = () => {
-  const toggleCounterHandler = () => {};
+function Counter() {
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
-};
+    const counter = useSelector(state => state.counter)
+    // params: state managed by redux, return the state you want to extract
+    // using useSelector automatically sets up a subscription to the Redux store for this component; component will be updated whenever the data changes in the Redux store
 
-export default Counter;
+    const dispatch = useDispatch()
+    // dispatch is now a function you can call to dispatch an action against the Redux store
+
+    const incrementHandler = () => {
+        dispatch({type: 'increment'})
+    }
+    const decrementHandler = () => {
+        dispatch({type: 'decrement'})
+    }
+    
+    const toggleCounterHandler = () => { }
+
+    return (
+        <main className={classes.counter}>
+            <h1>Redux Counter</h1>
+            <div className={classes.value}>{counter}</div>
+            <div>
+                <button onClick={incrementHandler}>Increment</button>
+                <button onClick={decrementHandler}>Decrement</button>
+            </div>
+            <button onClick={toggleCounterHandler}>Toggle Counter</button>
+        </main>
+    )
+}
+
+export default Counter
+
+
+// -------------------------------------------------
+// CLASS COMPONENT EXAMPLE
+// -------------------------------------------------
+// import {Component} from 'react'
+// import {connect} from 'react-redux'
+
+// class Counter extends Component {
+
+//     incrementHandler() {
+//         this.props.increment()
+//     }
+//     decrementHandler() {
+//         this.props.decrement()
+//     }
+//     toggleCounterHandler() {}
+
+//     render() {
+//         <main className={classes.counter}>
+//             <h1>Redux Counter</h1>
+//             <div className={classes.value}>{this.props.counter}</div>
+//             <div>
+//                 <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//                 <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//             </div>
+//             <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//         </main>
+//     }
+// }
+
+// const mapStateToProps = state => {
+//     return {
+//         counter: state.counter
+//     }
+// }
+        // maps redux state to props which will be received in this component then
+
+// const mapDispatchToProps = dispatch => {
+//     increment: () => dispatch({type: 'increment'})
+//     decrement: () => dispatch({type: 'decrement'})
+// } 
+        // store dispatch functions in props so that in the counter component, there are certain props which can be executed as a function, which will then, when executed, dispatch an action to the redux store
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter)
+
+        // when connect is executed, it returns a new function as a value which is then executed again, then pass the component (Counter) to the returned function as an argument
+        // connect will set up and manage a subscription to the redux store just like useDispatch & useSelector for functional components
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import {Provider} from 'react-redux'
+import store from './store/index'
 
-import './index.css';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+// store={store} provides the central data store to the app; now all components can tap into the store (get data out) and dispatch actions
